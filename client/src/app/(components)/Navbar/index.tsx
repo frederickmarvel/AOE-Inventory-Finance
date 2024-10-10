79d9699b18f67f5107dbf2e64b23d88f8e4238b4
Frederick Marvel <frederickmarvel@Fredericks-MacBook-Pro.local>
2024-10-10 21:10:38 +0700

    First Push

---
+++ client/src/app/(components)/Navbar/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {}
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center w-full mb-7'>Navbar</div>
+    {/* LEFT SIDE */}
+
+    <div className='flex justify-between items-center gap-5'></div>
+        <button className='px-3 bg-gray-100 rounded-full hover:bg-blue-100'></button>
+
+  )
+}
+
+export default Navbar
